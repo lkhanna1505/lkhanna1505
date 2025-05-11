97fb6f9974bac8c3d4b608364718a70a77ea7ff5
Lakshya <lakshyakhanna.11152005@gmail.com>
2025-05-11 19:10:47 +0530

    outer glow on lakshya

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({ scrollContainer }) => {
     <section className="parallax">
       <div className='parallax__content absolute top-[6%] sm:top-[10%] lg:top-[14%] w-full mx-auto lg:pl-[22vh] lg:pr-[18vh] xl:pl-60 xl:pr-44 2xl:px-24 3xl:px-36 flex flex-col lg:flex-row items-start z-10'>
         <div className="flex-1 lg:mb-0 pl-4 sm:pl-8 md:pl-12 lg:pl-16">
-          <h1 className='font-medium text-white font-[Orbitron] text-[24px] xs:text-[30px] sm:text-[40px] md:text-[48px] lg:text-[60px] 2xl:text-[108px] leading-[66px] 2xl:leading-[96px] mb-8 sm:mb-12 lg:mb-16'>
+          <h1 className='font-bold text-white font-[Creepster] text-[36px] xs:text-[48px] sm:text-[58px] md:text-[68px] lg:text-[80px] 2xl:text-[120px] leading-[80px] 2xl:leading-[120px] mb-8 sm:mb-12 lg:mb-16 streaky-glow'>
             LAKSHYA
           </h1>
           {/* <div className="scale-75 origin-top">
@@ -14,7 +14,7 @@ const Hero = ({ scrollContainer }) => {
           </div> */}
         </div>
         <div className="flex-1 flex justify-start lg:justify-end mt-8 sm:mt-16 md:mt-20 lg:mt-24 2xl:mt-32 ml-5 xs:ml-[-2vh] sm:ml-[-10vh] md:ml-[-16vh]">
-          <div className='font-bold text-[12px] sm:text-[18px] md:text-[22px] 2xl:text-[28px] sm:leading-[24px] md:leading-[30px] 2xl:leading-[36px] streaky-glow max-w-xs 2xl:max-w-md text-white text-left'>
+          <div className='font-bold text-[16px] sm:text-[24px] md:text-[30px] 2xl:text-[36px] sm:leading-[28px] md:leading-[36px] 2xl:leading-[44px] streaky-glow max-w-xs 2xl:max-w-md text-white text-left font-[Creepster]'>
             I love learning <br /> captivating experiences for the programming world to savor.
           </div>
         </div>
