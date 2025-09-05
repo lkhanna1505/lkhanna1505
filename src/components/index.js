3aa811c81e0cbbed3ffb8beec2c99ad68d42989a
Lakshya <137154151+lkhanna1505@users.noreply.github.com>
2025-09-05 16:53:33 +0530

    Changing images path

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,15 @@
-import close from "./close.svg";
-import logo from "./logo.svg";
-import menu from "./menu.svg";
+import close from "../assets/close.svg";
+import logo from "../assets/logo.svg";
+import menu from "../assets/menu.svg";
 
-import algorithms from "./algorithms.png";
-import devnotes from "./devnotes.png";
-import oscs from "./oscs.png";
-import lkdb from "./lkdb.png";
-import mavis from "./mavis.png";
-import model from "./MLmodel.png"
-import mkjournal from "./mkjournal.png"
-import lckiller from "./lckiller.png"
+import algorithms from "../assets/algorithms.png";
+import devnotes from "../assets/devnotes.png";
+import oscs from "../assets/oscs.png";
+import lkdb from "../assets/lkdb.png";
+import mavis from "../assets/mavis.png";
+import model from "../assets/MLmodel.png"
+import mkjournal from "../assets/mkjournal.png"
+import lckiller from "../assets/lckiller.png"
 // import mkjournal from "./mkjournal.png"
 
 export { algorithms, close, devnotes, logo, menu, oscs, lkdb, mavis, model, mkjournal, lckiller };
